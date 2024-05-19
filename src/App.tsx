a5885224ec2999505e539665180be3a8082c6b52
amanlal786 <contact.amanlall@gmail.com>
2024-05-19 12:18:16 -0700

    first push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
+// import Header from './components/Header';
+import About from './Components/About';
+// import Projects from './components/Projects';
+import Contact from './Components/Contact';
+// import Footer from './components/Footer';
+import MultiLayerParallax from './Components/MultiParallax';
+
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header /> */}
+      {/* <About /> */}
+      {/* <Projects /> */}
+      {/* <Contact /> */}
+      <MultiLayerParallax/>
+      <div className="w-full bg-[#06141D]">
+        <div className="max-w-lg space-y-4 mx-auto py-24 text-neutral-300">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+        </div>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this includes all the directories where your React components are located
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
